c6bdca6820a3382a20695b3e12986ada1b23d2a9
Chandan Roushan <charoush@publicisgroupe.net>
2019-10-15 14:14:59 +0530

    catalogue service

--- src/main/java/com/catalogue/Controller/GlobalExceptionHandler.java
+++ src/main/java/com/catalogue/Controller/GlobalExceptionHandler.java
@@ -4,15 +4,21 @@ import java.net.ConnectException;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler{
  
+	private static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+	
 	@ExceptionHandler(ConnectException.class)
-	public String handleSQLException(HttpServletRequest request, ConnectException ex){
-		System.out.println("---ConnectException While connecting to Inventory---");
-		return "errorfromadvice";
+	public ResponseEntity<String> handleSQLException(HttpServletRequest request, ConnectException ex){
+		logger.error("---ConnectException While connecting to Inventory---");
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 	}
 }
