8963fb9e116c136d6d26721ba2e4f421e099f789
krichava <krichava@publicisgroupe.net>
2019-10-17 10:19:13 +0530

    Hystrix Circuit breaker is incorporated for catalog-service

--- src/main/java/com/catalogue/service/CatalogueService.java
+++ src/main/java/com/catalogue/service/CatalogueService.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.sapient.ecomm_commons.domain.Product;
-
 
 @Component
 public class CatalogueService {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	public CatalogueService() {
 
@@ -25,15 +29,28 @@ public class CatalogueService {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@HystrixCommand(fallbackMethod = "getDefaultProducts")
 	public List<Product> getAllProducts() {
 		System.out.println("Inventory URI::::" + inventoryURI);
 		List<Product> products = (List<Product>) restTemplate.getForObject(inventoryURI, List.class);
 		return products;
 	}
 
+	public List<Product> getDefaultProducts() {
+
+		logger.error("inventory-service is not reachable, returning default products.");
+
+		List<Product> products = new ArrayList<>();
+
+		products.add(new Product("Book", 35D, "Default product"));
+		products.add(new Product("Bag", 440D, "Default product"));
+		products.add(new Product("Phone", 17000D, "Default product"));
+		return products;
+	}
+
 	public Product getProductById(String id) {
 		List<Product> productLists = new ArrayList<Product>();
-		//Request Inventory to fetch by Id below
+		// Request Inventory to fetch by Id below
 		Iterator<Product> iterator = productLists.iterator();
 		while (iterator.hasNext()) {
 			Product product = iterator.next();
@@ -46,7 +63,7 @@ public class CatalogueService {
 
 	public Product getProductByName(String name) {
 		List<Product> productLists = new ArrayList<Product>();
-		//Request Inventory to fetch by Name below
+		// Request Inventory to fetch by Name below
 		Iterator<Product> iterator = productLists.iterator();
 		while (iterator.hasNext()) {
 			Product product = iterator.next();
